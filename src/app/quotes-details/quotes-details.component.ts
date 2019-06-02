81329ef5451eb200f4055ca5c74f8ef59ea0bc87
Omtatah <murtallahomtatah@gmail.com>
2019-06-02 09:28:50 +0300

    properly punctuated my code by adding ({[ and ; where needed

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import{Quotes} from '../quotes';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import{Quotes} from '../quotes'
 @Component({
   selector: 'app-quotes-details',
   templateUrl: './quotes-details.component.html',
