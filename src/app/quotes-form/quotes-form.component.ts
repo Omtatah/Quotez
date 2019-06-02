96ab855cbc2bd3cf5c126dedb46a41a56a5899de
Omtatah <murtallahomtatah@gmail.com>
2019-06-02 08:49:38 +0300

    add the {Quotes} module from the '../quotes' and coded in the submitQuotes() function that carries on once the event emitter on the submit button is certified as true

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import{Quotes} from '../quotes'
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+
+   newQuotes=new Quotes('','','',new Date());
+   @Output() addQuotes=new EventEmitter<Quotes>();
+
+   submitQuotes(){
+     let quoteLength = this.newQuotes.name.length;
+     if(quoteLength<6){
+       alert("Hey, submit a quote");
+     }else{
+     this.addQuotes.emit(this.newQuotes);
+   }
+ }
 
   constructor() { }
 
