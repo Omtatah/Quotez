11781de9c8b26d1fd4b09e63bba578ab8e75b761
Omtatah <murtallahomtatah@gmail.com>
2019-06-02 07:49:49 +0300

    export class QuoteDetailsComponent and gave the quoteDeleting function a boolean type equated to true

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Quotes} from '../quotes';
 @Component({
   selector: 'app-quotes-details',
   templateUrl: './quotes-details.component.html',
   styleUrls: ['./quotes-details.component.css']
 })
 export class QuotesDetailsComponent implements OnInit {
+  @Input()quote:Quotes;
+  @Output()deleteit = new EventEmitter<boolean>();
+
+  quoteDeleting(deleting:boolean){
+    this.deleteit.emit(deleting);
+  }
+  
 
   constructor() { }
 
