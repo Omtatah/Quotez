7f097e4249ab561b27040609c15782b4f464d028
Omtatah <murtallahomtatah@gmail.com>
2019-06-02 07:27:27 +0300

    add initial quotes array to hold the quotes displayed in the app

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,6 +8,19 @@ import{Quotes} from '../quotes';
 })
 export class QuotesComponent implements OnInit {
 
+  quotes = [
+    new Quotes('Fly, if you cant fly run, if you cant run walk,if you cant walk crawl .','Raphael Katana',
+      ' Myles Munroe',new Date()),
+    new Quotes('Get busy living or get busy dying.Life is not a test till you start failing','Hamida Mstafa',' Mark Claine',
+        new Date()),
+    new Quotes('The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.',
+    'Boyd Ndonga',' Stephen King',new Date()),
+    new Quotes('Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.',
+       'Murtallah Omtatah',' Mark Twain',new Date()),
+    new Quotes('When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.',
+       'John Wanjema',' Audre Leon', new Date()),
+  ]
+  
   constructor() { }
 
   ngOnInit() {
