f062d1133b0ccf5310468f225c899a8217512700
Omtatah <murtallahomtatah@gmail.com>
2019-06-03 15:50:20 +0300

    specify the type of data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { DateCountPipe } from './date-count.pipe'
 
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
@@ -7,16 +9,21 @@ import { QuotesDetailsComponent } from './quotes-details/quotes-details.componen
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 import { VotesComponent } from './votes/votes.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuotesDetailsComponent,
     QuotesFormComponent,
-    VotesComponent
+    VotesComponent,
+    DateCountPipe,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
