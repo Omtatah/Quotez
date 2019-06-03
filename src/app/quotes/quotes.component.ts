414d68e7737fb08101af3c7ff73254b409d85906
Omtatah <murtallahomtatah@gmail.com>
2019-06-03 17:25:55 +0300

    perfect the .ts file

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -21,18 +21,18 @@ export class QuotesComponent implements OnInit {
       'John Wanjema', ' Audre Leon', new Date()),
   ]
 
-  deleteQuote(deleteit:any, index:any) {
+  deleteQuote(deleteit: any, index: any) {
     if (deleteit) {
       this.quotes.splice(index, 1);
     }
   }
 
-  toogleDetails(index:any) {
+  toogleDetails(index: any) {
     this.quotes[index].showInformation = !
       this.quotes[index].showInformation;
   }
 
-  addNewQuotes(quote:any) {
+  addNewQuotes(quote: any) {
     let quoteLength = this.quotes.length;
     quote.id = quoteLength + 1;
     quote.postDate = new Date(quote.postDate)
